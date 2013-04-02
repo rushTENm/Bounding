b2743fe3cc4c5c8216324ac53b4c2c5c206aefa0
Yong-il Cho <rushtenm@gmail.com>
2013-04-03 00:34:16 +0900

    World

--- Bounding/Bounding/Bounding/Game1.cs
+++ Bounding/Bounding/Bounding/Game1.cs
@@ -16,15 +16,41 @@ namespace Bounding
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        KeyboardState lastKeyboardState = new KeyboardState();
+        KeyboardState currentKeyboardState = new KeyboardState();
+
+        CameraTarget target;
+        ChaseCamera camera;
+
+        GameModel livingRoom;
+
+        bool cameraSpringEnabled = true;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1366;
+            graphics.PreferredBackBufferHeight = 768;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            target = new CameraTarget(GraphicsDevice);
+
+            livingRoom = new GameModel();
+
+            // Create the chase camera
+            camera = new ChaseCamera();
+
+            // Perform an inital reset on the camera so that it starts at the resting
+            // position. If we don't do this, the camera will start at the origin and
+            // race across the world to get behind the chased object.
+            // This is performed here because the aspect ratio is needed by Reset.
+            UpdateCameraChaseTarget();
+            camera.Reset();
 
             base.Initialize();
         }
@@ -35,6 +61,8 @@ namespace Bounding
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            target.Init(Content, 0.2f, @"JustBall");
+            livingRoom.Init(Content, 1.0f, @"LivingRoom");
         }
 
         protected override void UnloadContent()
@@ -49,8 +77,44 @@ namespace Bounding
                 this.Exit();
 
             // TODO: Add your update logic here
+            lastKeyboardState = currentKeyboardState;
+
+            currentKeyboardState = Keyboard.GetState();
+
+            // Pressing the A button or key toggles the spring behavior on and off
+            if (lastKeyboardState.IsKeyUp(Keys.A) && (currentKeyboardState.IsKeyDown(Keys.A)))
+            {
+                cameraSpringEnabled = !cameraSpringEnabled;
+            }
+
+            // Reset the ship on R key or right thumb stick clicked
+            if (currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                target.Reset();
+                camera.Reset();
+            }
+
+            // Update the ship
+            target.Update(gameTime);
+
+            // Update the camera to chase the new target
+            UpdateCameraChaseTarget();
+
+            // The chase camera's update behavior is the springs, but we can
+            // use the Reset method to have a locked, spring-less camera
+            if (cameraSpringEnabled)
+                camera.Update(gameTime);
+            else
+                camera.Reset();
 
             base.Update(gameTime);
+        }
+
+        private void UpdateCameraChaseTarget()
+        {
+            camera.ChasePosition = target.Position;
+            camera.ChaseDirection = target.Direction;
+            camera.Up = target.Up;
         }
 
         protected override void Draw(GameTime gameTime)
@@ -58,6 +122,10 @@ namespace Bounding
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            target.DrawMeshes(camera);
+            livingRoom.DrawMeshes(camera);
 
             base.Draw(gameTime);
         }
---
+++ Bounding/Bounding/Bounding/GameModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Bounding
+{
+    class GameModel
+    {
+        public Model Model;
+        public Vector3 Position = Vector3.Zero;
+        public BoundingSphere m_BoundingSphere;
+
+        float Scale;
+
+        public void Init(ContentManager content, float scale, string FileLoaction)
+        {
+            Model = content.Load<Model>(FileLoaction);
+            
+            Scale = scale;
+
+            m_BoundingSphere = Model.Meshes[0].BoundingSphere;
+            m_BoundingSphere.Radius = m_BoundingSphere.Radius * Scale;
+        }
+
+        public void DrawMeshes(ChaseCamera Camera)
+        {
+            Matrix[] boneTransforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(boneTransforms);
+
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+
+                    effect.World = boneTransforms[mesh.ParentBone.Index] * Matrix.CreateScale(Scale) * Matrix.CreateTranslation(Position);
+                    effect.Projection = Camera.Projection;
+                    effect.View = Camera.View;
+                }
+                mesh.Draw();
+            }
+        }
+
+    }
+}
