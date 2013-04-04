0df976056a28318f21c5a08b0a7c33ff0a0952fc
Yong-il Cho <rushtenm@gmail.com>
2013-04-04 09:58:10 +0900

    World

---
+++ Bounding/Bounding/Game1.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Bounding
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        KeyboardState lastKeyboardState = new KeyboardState();
+        KeyboardState currentKeyboardState = new KeyboardState();
+
+        CameraTarget target;
+        ChaseCamera camera;
+
+        GameModel livingRoom;
+
+        bool cameraSpringEnabled = true;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1366;
+            graphics.PreferredBackBufferHeight = 768;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            target = new CameraTarget(GraphicsDevice);
+
+            livingRoom = new GameModel();
+
+            // Create the chase camera
+            camera = new ChaseCamera();
+
+            // Perform an inital reset on the camera so that it starts at the resting
+            // position. If we don't do this, the camera will start at the origin and
+            // race across the world to get behind the chased object.
+            // This is performed here because the aspect ratio is needed by Reset.
+            UpdateCameraChaseTarget();
+            camera.Reset();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            target.Init(Content, 0.2f, @"JustBall");
+            livingRoom.Init(Content, 1.0f, @"LivingRoom");
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+            lastKeyboardState = currentKeyboardState;
+
+            currentKeyboardState = Keyboard.GetState();
+
+            // Pressing the A button or key toggles the spring behavior on and off
+            if (lastKeyboardState.IsKeyUp(Keys.A) && (currentKeyboardState.IsKeyDown(Keys.A)))
+            {
+                cameraSpringEnabled = !cameraSpringEnabled;
+            }
+
+            // Reset the ship on R key or right thumb stick clicked
+            if (currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                target.Reset();
+                camera.Reset();
+            }
+
+            // Update the ship
+            target.Update(gameTime);
+
+            // Update the camera to chase the new target
+            UpdateCameraChaseTarget();
+
+            // The chase camera's update behavior is the springs, but we can
+            // use the Reset method to have a locked, spring-less camera
+            if (cameraSpringEnabled)
+                camera.Update(gameTime);
+            else
+                camera.Reset();
+
+            base.Update(gameTime);
+        }
+
+        private void UpdateCameraChaseTarget()
+        {
+            camera.ChasePosition = target.Position;
+            camera.ChaseDirection = target.Direction;
+            camera.Up = target.Up;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            target.DrawMeshes(camera);
+            livingRoom.DrawMeshes(camera);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Bounding/Bounding/Octree.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Bounding
+{
+    /// 
+
+    /// Represents an octree spatial partioning system.
+    /// 
+
+    public class Octree
+    {
+        /// 
+
+        /// The number of children in an octree.
+        /// 
+
+        private const int ChildCount = 8;
+
+        /// 
+
+        /// The octree's looseness value.
+        /// 
+
+        private float looseness = 0;
+
+        /// 
+
+        /// The octree's depth.
+        /// 
+
+        private int depth = 0;
+
+        /// 
+
+        /// The octree's center coordinates.
+        /// 
+
+        private Vector3 center = Vector3.Zero;
+
+        /// 
+
+        /// The octree's length.
+        /// 
+
+        private float length = 0f;
+
+        /// 
+
+        /// The bounding box that represents the octree.
+        /// 
+
+        private BoundingBox bounds = default(BoundingBox);
+
+        /// 
+
+        /// The objects in the octree.
+        /// 
+
+        private List objects = new List();
+
+        /// 
+
+        /// The octree's child nodes.
+        /// 
+
+        private Octree[] children = null;
+
+        /// 
+
+        /// The octree's world size.
+        /// 
+
+        private float worldSize = 0f;
+
+        /// 
+
+        /// Creates a new octree.
+        /// 
+
+        /// The octree's world size.
+        /// The octree's looseness value.
+        /// The octree recursion depth.
+        public Octree(float worldSize, float looseness, int depth)
+            : this(worldSize, looseness, depth, 0, Vector3.Zero)
+        {
+        }
+
+        public Octree(float worldSize, float looseness, int depth, Vector3 center)
+            : this(worldSize, looseness, depth, 0, center)
+        {
+        }
+
+        /// 
+
+        /// Creates a new octree.
+        /// 
+
+        /// The octree's world size.
+        /// The octree's looseness value.
+        /// The maximum depth to recurse to.
+        /// The octree recursion depth.
+        /// The octree's center coordinates.
+        private Octree(float worldSize, float looseness,
+            int maxDepth, int depth, Vector3 center)
+        {
+            this.worldSize = worldSize;
+            this.looseness = looseness;
+            this.depth = depth;
+            this.center = center;
+            this.length = this.looseness * this.worldSize / (float)Math.Pow(2, this.depth);
+            float radius = this.length / 2f;
+
+            // Create the bounding box.
+            Vector3 min = this.center + new Vector3(-radius);
+            Vector3 max = this.center + new Vector3(radius);
+            this.bounds = new BoundingBox(min, max);
+
+            // Split the octree if the depth hasn't been reached.
+            if (this.depth < maxDepth)
+            {
+                this.Split(maxDepth);
+            }
+        }
+
+        /// 
+
+        /// Removes the specified obj.
+        /// 
+
+        /// The obj.
+        public void Remove(T obj)
+        {
+            objects.Remove(obj);
+        }
+
+        /// 
+
+        /// Determines whether the specified obj has changed.
+        /// 
+
+        /// The obj.
+        /// The transformebbox.
+        /// 
+        ///   true if the specified obj has changed; otherwise, false.
+        /// 
+        public bool HasChanged(T obj, BoundingBox transformebbox)
+        {
+            return this.bounds.Contains(transformebbox) == ContainmentType.Contains;
+        }
+
+        /// 
+
+        /// Stills inside ?
+        /// 
+
+        /// The o.
+        /// The center.
+        /// The radius.
+        /// 
+        public bool StillInside(T o, Vector3 center, float radius)
+        {
+            Vector3 min = center - new Vector3(radius);
+            Vector3 max = center + new Vector3(radius);
+            BoundingBox bounds = new BoundingBox(min, max);
+
+            if (this.children != null)
+                return false;
+
+            if (this.bounds.Contains(bounds) == ContainmentType.Contains)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// 
+
+        /// Stills inside ?.
+        /// 
+
+        /// The obj.
+        /// Its bounds.
+        /// 
+        public bool StillInside(T o, BoundingBox bounds)
+        {
+            if (this.children != null)
+                return false;
+
+            if (this.bounds.Contains(bounds) == ContainmentType.Contains)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// 
+
+        /// Adds the given object to the octree.
+        /// 
+
+        /// The object to add.
+        /// The object's center coordinates.
+        /// The object's radius.
+        public Octree Add(T o, Vector3 center, float radius)
+        {
+            Vector3 min = center - new Vector3(radius);
+            Vector3 max = center + new Vector3(radius);
+            BoundingBox bounds = new BoundingBox(min, max);
+
+            if (this.bounds.Contains(bounds) == ContainmentType.Contains)
+            {
+                return this.Add(o, bounds, center, radius);
+            }
+            return null;
+        }
+
+
+        /// 
+
+        /// Adds the given object to the octree.
+        /// 
+
+        public Octree Add(T o, BoundingBox transformebbox)
+        {
+            float radius = (transformebbox.Max - transformebbox.Min).Length() / 2;
+            Vector3 center = (transformebbox.Max + transformebbox.Min) / 2;
+
+            if (this.bounds.Contains(transformebbox) == ContainmentType.Contains)
+            {
+                return this.Add(o, transformebbox, center, radius);
+            }
+            return null;
+        }
+
+
+        /// 
+
+        /// Adds the given object to the octree.
+        /// 
+
+        /// The object to add.
+        /// The object's bounds.
+        /// The object's center coordinates.
+        /// The object's radius.
+        private Octree Add(T o, BoundingBox bounds, Vector3 center, float radius)
+        {
+            if (this.children != null)
+            {
+                // Find which child the object is closest to based on where the
+                // object's center is located in relation to the octree's center.
+                int index = (center.X <= this.center.X ? 0 : 1) +
+                    (center.Y >= this.center.Y ? 0 : 4) +
+                    (center.Z <= this.center.Z ? 0 : 2);
+
+                // Add the object to the child if it is fully contained within
+                // it.
+                if (this.children[index].bounds.Contains(bounds) == ContainmentType.Contains)
+                {
+                    return this.children[index].Add(o, bounds, center, radius);
+
+                }
+            }
+            this.objects.Add(o);
+            return this;
+        }
+
+        /// 
+
+        /// Draws the octree.
+        /// 
+
+        /// The viewing matrix.
+        /// The projection matrix.
+        /// The objects in the octree.
+        /// The number of octrees drawn.
+        public int Draw(Matrix view, Matrix projection, List objects)
+        {
+            BoundingFrustum frustum = new BoundingFrustum(view * projection);
+            ContainmentType containment = frustum.Contains(this.bounds);
+
+            return this.Draw(frustum, view, projection, containment, objects);
+        }
+
+        /// 
+
+        /// Draws the octree.
+        /// 
+
+        /// The viewing frustum used to determine if the octree is in view.
+        /// The viewing matrix.
+        /// The projection matrix.
+        /// Determines how much of the octree is visible.
+        /// The objects in the octree.
+        /// The number of octrees drawn.
+        private int Draw(BoundingFrustum frustum, Matrix view, Matrix projection,
+            ContainmentType containment, List objects)
+        {
+            int count = 0;
+
+            if (containment != ContainmentType.Contains)
+            {
+                containment = frustum.Contains(this.bounds);
+            }
+
+            // Draw the octree only if it is atleast partially in view.
+            if (containment != ContainmentType.Disjoint)
+            {
+                // Draw the octree's bounds if there are objects in the octree.
+                if (this.objects.Count > 0)
+                {
+                    if (DebugDraw != null)
+                        DebugDraw.AddShape(new DebugBox(this.bounds, Color.White));
+                    objects.AddRange(this.objects);
+                    count++;
+                }
+
+                // Draw the octree's children.
+                if (this.children != null)
+                {
+                    foreach (Octree child in this.children)
+                    {
+                        count += child.Draw(frustum, view, projection, containment, objects);
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// 
+
+        /// Splits the octree into eight children.
+        /// 
+
+        /// The maximum depth to recurse to.
+        private void Split(int maxDepth)
+        {
+            this.children = new Octree[Octree.ChildCount];
+            int depth = this.depth + 1;
+            float quarter = this.length / this.looseness / 4f;
+
+            this.children[0] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(-quarter, quarter, -quarter));
+            this.children[1] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(quarter, quarter, -quarter));
+            this.children[2] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(-quarter, quarter, quarter));
+            this.children[3] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(quarter, quarter, quarter));
+            this.children[4] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(-quarter, -quarter, -quarter));
+            this.children[5] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(quarter, -quarter, -quarter));
+            this.children[6] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(-quarter, -quarter, quarter));
+            this.children[7] = new Octree(this.worldSize, this.looseness,
+                maxDepth, depth, this.center + new Vector3(quarter, -quarter, quarter));
+        }
+
+    }
+}
